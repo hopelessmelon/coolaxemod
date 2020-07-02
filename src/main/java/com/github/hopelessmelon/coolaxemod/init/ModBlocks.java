0716fd6fcdab4ef9c21d622e753975215630ab7f
hopelessmelon <hopelessmelon@gmail.com>
2020-07-01 21:46:47 -0600

    texture works for Block and BlockItem, but only when all sides are the same texture right now.

--- src/main/java/com/github/hopelessmelon/coolaxemod/init/ModBlocks.java
+++ src/main/java/com/github/hopelessmelon/coolaxemod/init/ModBlocks.java
@@ -1,6 +1,7 @@
 /*
  This file is the same as the ModItems file, but for blocks. The tutorial has run out so I need to find another or figure this out on my own.
  Hopefully it's pretty similar to the process with Items and I can just follow the same steps here.
+ 7/1: I don't think I actually need this.
  */
 
 package com.github.hopelessmelon.coolaxemod.init;
